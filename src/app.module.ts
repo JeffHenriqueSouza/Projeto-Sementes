c20ade63a309b6007b837a9e7cb84d40ddff4f56
JeffHenriqueSouza <jeffdeveloperfullstack@gmail.com>
2024-05-21 15:23:06 -0300

    seventeenth7 commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './users/usuario.module'; 
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AvaliacaoModule } from './avaliacao/avaliacao.module';
 import { FeedbackModule } from './feedback/feedback.module'; 
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-    }),
+    ConfigModule.forRoot(),
     UsuarioModule, 
     DatabaseModule,
     AvaliacaoModule,
